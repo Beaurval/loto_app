593c5b0d442e70d96a2c7ed98f3d52c796a3d4c9
Beaurval <beaury.valentin@gmail.com>
2021-01-08 00:27:23 +0100

    Barre de navigation inférieure

---
+++ lib/views/homepage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+
+class HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
+            label: 'Historique',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: 'Probabilités',
+          ),
+        ],
+        selectedItemColor: Theme.of(context).buttonColor,
+        unselectedItemColor: Theme.of(context).unselectedWidgetColor,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Theme.of(context).primaryColor,
+
+      ),
+    );
+  }
+}
